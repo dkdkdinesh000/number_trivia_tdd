6ab96c4755b2c98c0e072b56289acbedbeb88692
dkdkdinesh000 <42449860+dkdkdinesh000@users.noreply.github.com>
2022-07-31 13:32:38 +0530

    Get Concrete and Get random number trivia was added

---
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia_tdd/features/core/error/failures.dart';
+import 'package:number_trivia_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure, NumberTrivia>> getConcreteNumber(int number);
+  Future<Either<Failure, NumberTrivia>> getRandomNumber();
+}
---
+++ test/features/number_trivia/domain/use_case/get_concreate_number_trivia_test.dart
@@ -0,0 +1,36 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:number_trivia_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:number_trivia_tdd/features/number_trivia/domain/usecases/get_concreate_number_trivia.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+void main() {
+  late GetConcreteNumberTrivia usecase;
+  late MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  final tNumber = 1;
+  final tNumberTrivia = const NumberTrivia(text: "test", number: 1);
+
+  test("should get trivia for the number from the repository", () async {
+    //arrange
+    when(() => mockNumberTriviaRepository.getConcreteNumber(tNumber))
+        .thenAnswer((_) async => Right(tNumberTrivia));
+
+    //act
+    final result = await usecase.execute(number: tNumber);
+
+    //assett
+    expect(result, Right(tNumberTrivia));
+    verify(() => mockNumberTriviaRepository.getConcreteNumber(tNumber))
+        .called(1);
+  });
+}
