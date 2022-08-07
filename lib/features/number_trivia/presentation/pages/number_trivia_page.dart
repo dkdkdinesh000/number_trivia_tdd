62c7fc618213cf6e6e9686160b2badcd391c8650
dkdkdinesh000 <42449860+dkdkdinesh000@users.noreply.github.com>
2022-08-07 18:41:29 +0530

    some code refactoring was done

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:number_trivia_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia_tdd/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 
 import '../../../../injection_container.dart';
+import '../widgets/widgets.dart';
 
 class NumberTriviaPage extends StatelessWidget {
   const NumberTriviaPage({Key? key}) : super(key: key);
@@ -56,147 +56,5 @@ class NumberTriviaPage extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class MessageDisplay extends StatelessWidget {
-  final String message;
-  const MessageDisplay({Key? key, required this.message}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height / 3,
-      child: Center(
-        child: SingleChildScrollView(
-          child: Text(
-            message,
-            style: const TextStyle(fontSize: 25),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class LoadingWidget extends StatelessWidget {
-  const LoadingWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height / 3,
-      child: const Center(child: CircularProgressIndicator()),
-    );
-  }
-}
-
-class TriviaDisplay extends StatelessWidget {
-  final NumberTrivia numberTrivia;
-
-  const TriviaDisplay({
-    Key? key,
-    required this.numberTrivia,
-  })  : assert(numberTrivia != null),
-        super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height / 3,
-      child: Column(
-        children: <Widget>[
-          // Fixed size, doesn't scroll
-          Text(
-            numberTrivia.number.toString(),
-            style: const TextStyle(
-              fontSize: 50,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          // Expanded makes it fill in all the remaining space
-          Expanded(
-            child: Center(
-              // Only the trivia "message" part will be scrollable
-              child: SingleChildScrollView(
-                child: Text(
-                  numberTrivia.text,
-                  style: const TextStyle(fontSize: 25),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class TriviaControls extends StatefulWidget {
-  const TriviaControls({Key? key}) : super(key: key);
-
-  @override
-  State<TriviaControls> createState() => _TriviaControlsState();
-}
-
-class _TriviaControlsState extends State<TriviaControls> {
-  final controller = TextEditingController();
-  String? inputStr;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextField(
-          controller: controller,
-          keyboardType: TextInputType.number,
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            hintText: 'Input a number',
-          ),
-          onChanged: (value) {
-            inputStr = value;
-          },
-          onSubmitted: (_) {
-            dispatchConcrete();
-          },
-        ),
-        const SizedBox(height: 10),
-        Row(
-          children: <Widget>[
-            Expanded(
-              child: RaisedButton(
-                child: Text('Search'),
-                color: Theme.of(context).accentColor,
-                textTheme: ButtonTextTheme.primary,
-                onPressed: dispatchConcrete,
-              ),
-            ),
-            const SizedBox(width: 10),
-            Expanded(
-              child: RaisedButton(
-                child: Text('Get random trivia'),
-                onPressed: dispatchRandom,
-              ),
-            ),
-          ],
-        )
-      ],
-    );
-  }
-
-  void dispatchConcrete() {
-    // Clearing the TextField to prepare it for the next inputted number
-    controller.clear();
-    BlocProvider.of<NumberTriviaBloc>(context)
-        .add(GetTriviaForConcreteNumber(inputStr ?? ""));
-  }
-
-  void dispatchRandom() {
-    controller.clear();
-    BlocProvider.of<NumberTriviaBloc>(context).add(GetTriviaForRandomNumber());
   }
 }
